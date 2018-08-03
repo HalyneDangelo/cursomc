25930ed643afafcd80f4b6af061c93c590a2669a
Halyne Ribeiro <halyne.dangelo@gmail.com>
2018-08-03 10:41:29 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/halyne/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.halyne.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.halyne.cursomc.domain.Pedido;
+import com.halyne.cursomc.repositories.PedidoRepository;
+import com.halyne.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
