180fd6f4897c4fe8cd6a7fc445cf58334af66782
Halyne Ribeiro <halyne.dangelo@gmail.com>
2018-08-13 11:08:16 -0300

    Inserindo Pedido

--- src/main/java/com/halyne/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/halyne/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.halyne.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.halyne.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
